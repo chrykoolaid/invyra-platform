c81acde5c022c14803265de02b706b812da2e0b9
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-14 16:57:07 +0800

    PF2 Sprint 1: add API root router

---
+++ src/invyra_platform/api/router.py
@@ -0,0 +1,8 @@
+"""Root API router."""
+
+from fastapi import APIRouter
+
+from invyra_platform.api.v1.router import router as v1_router
+
+api_router = APIRouter()
+api_router.include_router(v1_router, prefix="/api/v1")
